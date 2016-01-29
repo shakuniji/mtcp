e6040977d9bf8dbfea5e82ca1519fc5961cf39eb
root <root@paravance-64.rennes.grid5000.fr>
2016-01-29 15:20:18 +0100

    fix

--- client.py
+++ client.py
@@ -12,24 +12,27 @@ def sendserver(x):
 	BUFFER_SIZE = 1024
 	MESSAGE = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
 	
-	dt = datetime.datetime(2016,1,29,15,13,30)                                                      
+	dt = datetime.datetime(2016,1,29,15,22,30)                                                      
 	while (datetime.datetime.now() < dt):continue
 	
 	endTime = datetime.datetime.now() + datetime.timedelta(minutes=1)
 	count=0
 	while(datetime.datetime.now() <= endTime):
-		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		#s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-		l_onoff = 1
-		l_linger = 0
-		s.setsockopt(socket.SOL_SOCKET, socket.SO_LINGER,struct.pack('ii', l_onoff, l_linger))
-		#s.bind(("10.0.0.5",0))
-		s.connect((TCP_IP, TCP_PORT))
-		s.send(MESSAGE)
-		data = s.recv(BUFFER_SIZE)
-		count+=1
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+			#s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+			l_onoff = 1
+			l_linger = 0
+			s.setsockopt(socket.SOL_SOCKET, socket.SO_LINGER,struct.pack('ii', l_onoff, l_linger))
+			#s.bind(("10.0.0.5",0))
+			s.connect((TCP_IP, TCP_PORT))
+			s.send(MESSAGE)
+			data = s.recv(BUFFER_SIZE)
+			count+=1
 
-		#s.close()
+			#s.close()
+		except:
+			print 'e'
 	print "Requests per second ", count/60
 	return count/60
 
